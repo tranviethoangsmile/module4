513ddd4a80fe156c5233c16b1d95bdc616fefa89
Trần Viết Hoàng <81605215+tranviethoangsmile@users.noreply.github.com>
2021-10-18 21:02:26 +0700

    update

---
+++ TH-ValidateForm/src/main/java/com/codegym/model/User.java
@@ -0,0 +1,41 @@
+package com.codegym.model;
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+public class User{
+    @NotBlank(message = "{name_errors}")
+    private String name;
+
+    private int age;
+
+    public User() {
+    }
+
+    public User( String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+}
